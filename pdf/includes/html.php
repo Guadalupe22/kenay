a96ba844b272e3e8c3ac81f9814f2c6dcced04ca
Guadalupe22 <lupitavasquez1995@hotmail.com>
2015-06-10 12:40:46 -0600

    mi primer comit

---
+++ pdf/includes/html.php
@@ -0,0 +1,15 @@
+<html>         <body>   <h1>Este es nuestro pequeño proyecto de aplicaciones web</h1>
+<h2>Necesitamos pasar el grado</h2>
+
+<h2>Jose Henrriquez Chavarria</h2>
+<h2>tecnico en ingenieria de sistemas informaticos</h2>
+<h2>Sistemas 22</h2>
+
+<h2> Nombre de los intergantes </h2>
+<h3>Kevin Adonay Baires Peña </h3>
+<h3>Berta Guadalupe Vasquez Martinez</h3>
+<h3>Douglas Ernesto Hernandez Duran</h3>
+<h3>Karla Maria Serrano Garcia </h3>
+<h3>Cristan Alexander  Gonzalez Puro</h3>
+<h3> <p>Mi Primer PDF</p> <p>esperon pasar con esto </p>  </h3>
+                 </body> </html> 
